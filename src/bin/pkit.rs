0033935d7ba2559d7c956a98e21270c7500c83db
GamerBoss101 <adithcool.5@gmail.com>
2025-03-16 16:34:50 -0400

    Request Module

--- src/bin/pkit.rs
+++ src/bin/pkit.rs
@@ -1,10 +1,17 @@
 use std::env;
 
 use pkit::parser;
+use pkit::request;
 
 // PATH="$(pwd):$PATH"
 
-fn main() {
+async fn run() {
+    let url = "https://www.dropbox.com/scl/fi/qmny11w7wkjnskyh7g76m/Cobblemon-Official-Fabric-1.0.0.mrpack?rlkey=s3nkxs0u18s123a4pylvmphzn&st=36q91qxa&dl=1";
+    request::download(url).await;
+}
+
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
     let cli = parser::main(&args[1..]);
 
@@ -13,4 +20,6 @@ fn main() {
     for flag in cli.flags {
         println!("{:?} - {:?}", flag.flag, flag.value);
     }
+
+    run().await;
 }
---
+++ src/request.rs
@@ -0,0 +1,42 @@
+
+use std::fs;
+
+use reqwest;
+
+pub async fn get(url: &str) {
+    let client: reqwest::Client = reqwest::Client::new();
+
+    let resp: reqwest::Response = client.get(url)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    println!("Status: {:?}", resp.status());
+}
+
+pub async fn post(url: &str) {  
+    let client: reqwest::Client = reqwest::Client::new();
+
+    let resp: reqwest::Response = client.post(url)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    println!("Status: {:?}", resp.status());
+}
+
+pub async fn download(url: &str) {
+    let client: reqwest::Client = reqwest::Client::new();
+
+    let resp: reqwest::Response = client.get(url)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    println!("Status: {:?}", resp.status());
+
+
+    let bytes = resp.bytes().await.expect("Failed to read response");
+    let filename = url.split("/").last().unwrap().split("?").next().unwrap();
+    fs::write(filename, bytes).expect("Failed to write file");
+}
